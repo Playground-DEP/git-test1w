a4695d0ae6add2add9aef8379d824ca56424725e
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-12 10:48:19 +0530

    Completed

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World...Fuck");
+        System.out.println("dhsgfjihkhps");
+        System.out.println("sgfihaoj opk[y");
+    }
+}
